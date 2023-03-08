634c09ff10d8964c6c71784c624dc6bb32068bd5
Josh Stallnick <violabs.software@gmail.com>
2023-03-07 22:05:39 -0600

    Remove native stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.violabs"
-version = "1.1-SNAPSHOT"
+version = "1.1.1"
 
 repositories {
     mavenCentral()
@@ -31,14 +31,6 @@ kotlin {
             }
         }
     }
-    val hostOs = System.getProperty("os.name")
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
 
     sourceSets {
         val commonMain by getting {
@@ -59,7 +51,5 @@ kotlin {
         }
         val jsMain by getting
         val jsTest by getting
-        val nativeMain by getting
-        val nativeTest by getting
     }
 }
